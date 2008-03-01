4e2fc3afcc237187582705303be606d370f4452f
ossareh <ossareh@307f3d4c-5847-0410-938a-ad9b0fe8feb3>
2008-03-01 06:40:01 +0000

    moved away from pysvn and used the svn libs that ship as part of subversion also fixed broken HTTP auth
    git-svn-id: http://twitvn.googlecode.com/svn/trunk@3 307f3d4c-5847-0410-938a-ad9b0fe8feb3

--- twitvn.py
+++ twitvn.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 from optparse import OptionParser
-from urllib2 import Request, urlopen, URLError, HTTPError, HTTPBasicAuthHandler, build_opener, install_opener, HTTPPasswordMgrWithDefaultRealm
+from urllib2 import Request, HTTPPasswordMgrWithDefaultRealm, build_opener, install_opener, urlopen, HTTPBasicAuthHandler
 import urllib
-import os
-from pysvn import Revision, Client, opt_revision_kind
+import svn.repos
+import svn.fs
+import svn.core
 
 version = '0.1a'
 
@@ -17,98 +18,85 @@ class TwitHTTP:
                 self.__username = username
                 self.__password = password
 
+
         def sendTwitter(self, message):
-                twitAuth = HTTPBasicAuthHandler()
-                twitAuth.add_password('Twitter API', 'http://twitter.com/statuses/',username, password)
-                opener = build_opener(twitAuth)
-                install_opener(opener)
-                values = {'status' : message}
-                data = urllib.urlencode(values)
+                req = Request('http://twitter.com/statuses/update.json')
+                twitAuth = HTTPPasswordMgrWithDefaultRealm()
+                twitAuth.add_password(None, 'twitter.com', self.__username, self.__password)
+                # install the auth handler built from the HTTPPasswordMgrWithDefaultRealm password manager
+                install_opener(build_opener(HTTPBasicAuthHandler(twitAuth)))
+                data = urllib.urlencode({'status' : message})
                 try:
-                        f = urlopen(req)
+                        urlopen(req, data)
                 except IOError, e:
                         print 'We got an error'
                         print e.code
 
 class SVNHelper:
         "This class interacts with SVN for us"
-        author = None
-        message = None
 
-        def __init__(self, path, revision):
-                client = Client(path)
-                log_message = client.log('/', revision_start=Revision(opt_revision_kind.number, revision), limit=1).pop()
-                self.auth = log_message.author
-                self.message = log_message.message
+        def __init__(self, path, revision, pool):
+                repos_ptr = svn.repos.svn_repos_open(path, pool)
+                fs_ptr = svn.repos.svn_repos_fs(repos_ptr)
+                self.message = svn.fs.revision_prop(fs_ptr, revision, svn.core.SVN_PROP_REVISION_LOG, pool)
+                self.author = svn.fs.revision_prop(fs_ptr, revision, svn.core.SVN_PROP_REVISION_AUTHOR, pool)
 
 
 # All the main stuff happens here meng.. 
 
 def generateTwitter(author, revision, comment, domain):
-        trimLength = 140 - (len(author)+1) - len(domain) - len('/changeset/') - len(revision) - 5
+        trimLength = 140 - (len(author)+1) - len(domain) - len('/changeset/') - len(str(revision)) - 5
         if len(comment) > trimLength:
                 comment = comment[0:trimLength].rstrip(' ') + '...'
         return '%s: %s %s/changeset/%s' % (author, comment, domain, revision)
 
-def processArgs():
+def main(pool, options):
+        svnHelper = SVNHelper(options.PATH, options.REVISION, pool)
+
+        twitter = generateTwitter(svnHelper.author, options.REVISION, svnHelper.message, options.DOMAIN)
+
+        #print twitter
+
+        TwitHTTP(options.USERNAME, options.PASSWORD).sendTwitter(twitter)
+
+if __name__ == '__main__':
         # get arguments from the command line
         # important arguments are username, password, comment
 
         usage = 'usage: %prog -u<username> -p<password> -f<svn_path> -r<svn_revision> -t<trac_url>'
 
         parser = OptionParser(usage=usage,version='%prog: ' + version)
-        parser.add_option('-u', '--twitter-username', dest='username', type='string',
+        parser.add_option('-u', '--twitter-username', dest='USERNAME', type='string',
                 help='Twitter Account Username',
                 action='store')
-        parser.add_option('-p', '--twitter-password', dest='password', type='string',
+        parser.add_option('-p', '--twitter-password', dest='PASSWORD', type='string',
                 help='Twitter Account Password',
                 action='store')
-        parser.add_option('-f', '--svn-path', dest='path', type='string',
+        parser.add_option('-f', '--svn-path', dest='PATH', type='string',
                 help='SVN Repository Path',
                 action='store')
-        parser.add_option('-r', '--svn-revision', dest='revision', type='string',
+        parser.add_option('-r', '--svn-revision', dest='REVISION', type='int',
                 help='SVN Revision',
                 action='store')
-        parser.add_option('-t', '--tracurl', dest='domain', type='string',
+        parser.add_option('-t', '--tracurl', dest='DOMAIN', type='string',
                 help='Trac URL',
                 action='store')
 
         (options, args) = parser.parse_args()
 
-        if options.username is None:
+        if options.USERNAME is None:
                 parser.error('Twitter username must be set')
-        else:
-                username = options.username
 
-        if options.password is None:
+        if options.PASSWORD is None:
                 parser.error('Twitter password must be set')
-        else:
-                password = options.password
 
-        if options.path is None:
+        if options.PATH is None:
                 parser.error('Subversion repo path must be set')
-        else:
-                path = options.path
 
-        if options.revision is None:
+        if options.REVISION is None:
                 parser.error('subversion revision must be set')
-        else:
-                revision = options.revision
 
-        if options.domain is None:
+        if options.DOMAIN is None:
                 parser.error('Trac Domain Name must be set')
-        else:
-                domain = options.domain
 
-        return (username, password, path, revision, domain)
-
-(username, password, path, revision, domain) = processArgs()
-svnHelper = SVNHelper(path, revision)
-if svnHelper.author is None:
-        svnHelper.author = os.environ['USER']
-
-twitter = generateTwitter(svnHelper.author, revision, svnHelper.message, domain)
-
-print twitter
-
-#TwitHTTP(username, password).sendTwitter(twitter)
+        svn.core.run_app(main, options=options)
