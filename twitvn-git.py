9f391427de64d0f4f1435ef33de9a95be18dffee
NTICompass <NTICompass@307f3d4c-5847-0410-938a-ad9b0fe8feb3>
2011-06-19 23:51:07 +0000

    Fixed twitvn-git so it tweets all commits from a push
    git-svn-id: http://twitvn.googlecode.com/svn/trunk@24 307f3d4c-5847-0410-938a-ad9b0fe8feb3

--- twitvn-git.py
+++ twitvn-git.py
@@ -33,7 +33,7 @@ class TwitOAuth:
 
 	def __init__(self, message):
 		self.__tweet = message
-		
+
 	def sendTwitter(self):
 		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 		auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -42,45 +42,66 @@ class TwitOAuth:
 
 class GITHelper:
 	"This class interacts with GIT for us"
-	def __init__(self, path):
-		repo = git.Repo(path)
-		headcommit = repo.head.commit
+	def __init__(self, headcommit):
 		self.message = headcommit.message
 		self.author = headcommit.author.name
 
+def reverseIter(obj):
+	revObj = []
+	for x in obj:
+		revObj.append(x)
+	revObj.reverse()
+	return revObj
 
 def generateTwitter(author, comment):
 	tweet = ''
 	tracurl = ''
 	trimLength = 140 - (len(author)+1) - 5
-		
+
 	if len(comment) > trimLength:
 		comment = comment[0:trimLength].rstrip(' ') + '...'
-		
+
 	tweet = '%s: %s %s' % (author, comment, tracurl)
 	return tweet
 
 def main(options):
-	# Get SVN info
-	gitHelper = GITHelper(options.PATH)
-	# Generate a tweet
-	twitter = generateTwitter(gitHelper.author, gitHelper.message)
-	# Send it to twitter
-	TwitOAuth(twitter).sendTwitter()
+	# Get GIT info
+	looping = False
+	repo = git.Repo(options.PATH)
+	for head in reverseIter(repo.iter_commits('master')):
+		if looping == False:
+			if head.hexsha == options.OLD:
+				looping = True
+			continue
+		gitHelper = GITHelper(head)
+		# Generate a tweet
+		twitter = generateTwitter(gitHelper.author, gitHelper.message)
+		# Send it to twitter
+		TwitOAuth(twitter).sendTwitter()
+		if head.hexsha == options.NEW:
+			break
 
 if __name__ == '__main__':
 	# get arguments from the command line
 
-	usage = 'usage: %prog -f<git_path>'
+	usage = 'usage: %prog -f<git_path> -o<oldref> -n<newref>'
 
 	parser = OptionParser(usage=usage,version='%prog: ' + version)
-	
+
 	parser.add_option('-f', '--git-path', dest='PATH', type='string',
 		help='GIT Repository Path', action='store')
+	parser.add_option('-o', '--oldref', dest='OLD', type='string',
+		help='Old head ref', action='store')
+	parser.add_option('-n', '--newref', dest='NEW', type='string',
+		help='New head ref', action='store')
 
 	(options, args) = parser.parse_args()
 
 	if options.PATH is None:
 		parser.error('GIT repo path must be set')
+	if options.OLD is None:
+		parser.error('Old ref must be set')
+	if options.NEW is None:
+		parser.error('New ref must be set')
 
 	main(options)
